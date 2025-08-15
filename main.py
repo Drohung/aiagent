760791e073ca3ccd80d433ed9257b26f588ab9f4
Drohung <rhemliv@gmail.com>
2025-08-14 20:36:26 -0400

    Added function calls

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 
 def main():
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -14,17 +24,28 @@ def main():
     parser.add_argument('user_prompt')
     parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
     messages = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
     ]
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
+        model="gemini-2.0-flash-001",
+        contents=messages, 
+        config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
     )
     if args.verbose:
         print("User prompt:", args.user_prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if response.candidates[0].content.parts[0].function_call:
+        function_call = response.candidates[0].content.parts[0].function_call
+        print(f"Calling function: {function_call.name}({function_call.args})")
     print("response:")
     print(response.text)
 
